abe87b533f2b95286ebbf8a2a7349d2235872cd1
joaoschoen <joaoschoen@gmail.com>
2025-07-02 02:44:58 -0300

    a

--- configs/recommended.js
+++ configs/recommended.js
@@ -1,5 +1,9 @@
+import code_safety from "../lib"
+
 module.exports = {
-      plugins: ['code-safety'],
+      plugins: {
+        "code-safety": code_safety
+      },
       rules: {
         'code-safety/"no-uncaught-async': 'error',
         'code-safety/"no-uncaught-eval': 'error',
